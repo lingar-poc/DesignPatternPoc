a0cedd263f62e4698fb710d54960e78a32e05084
Administrator <Administrator@4115-TEACHER>
2024-09-20 10:43:56 +0300

    Prodcut builder

---
+++ app/IProductBuilder.cs
@@ -0,0 +1,20 @@
+﻿using c_sharp_apps_Akiva_Cohen.bank_app;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternPoc.app
+{
+    public interface IProductBuilder
+    {
+        void BuildName();
+        void BuildPackage();
+        void BuildPrice();
+        void BuildManufacture();
+
+
+
+    }
+}
